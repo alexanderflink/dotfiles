ebd6d73f0483c2b499e04789bbdd69e666938a6c
Alexander Flink <alexanderflink@alexanders-mbp.lan>
2022-07-14 10:51:41 +0200

    add git aliases

--- shell-setup.sh
+++ shell-setup.sh
@@ -6,3 +6,11 @@
 
 # lazygit
 alias lg="lazygit" -s
+
+# git
+alias gs="git status" -s
+alias gpl="git pull" -s
+alias gps="git pull" -s
+alias gf="git fetch" -s
+alias ga="git add" -s
+alias gc="git commit" -s
